b4a418daeb13da084f262b09f55fe41b09e9555b
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-21 15:30:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,18 +23,18 @@ def valid_move?(board, index)
 end
 
 def move(board, index, character = "X")
-  is_valid = valid_move?(board, index)
-  if is_valid
-    board[index] = character
-  end
+  board[index] = character
   display_board(board)
 
 end
 
 def turn(board)
-  display_board(board)
   puts "Please enter 1-9:"
   index = input_to_index(gets.strip)
 
-  move(board, index)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+  end
+  
 end
